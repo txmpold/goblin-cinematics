eb532f9d4c160efa15db29180f77c83a257d7069
txmpold <love.tampold@hotmail.com>
2026-04-01 16:49:17 +0200

    create barebone about page

--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -1,11 +1,22 @@
 import Header from "../components/Header";
+import "./about-page.css";
 
 export default function AboutPage() {
   return (
     <>
-      <Header pageClass="other-header" />
+      <Header />
       <main>
-        <h1>About Page</h1>
+        <section className="about-content">
+          <div className="about-container">
+            <h1>About Us</h1>
+            <p>
+              Passionate team of 2 based in Stockholm, Sweden, crafting
+              captivating videos for your storytelling needs. Let us bring your
+              vision to life with professional cinematography and
+              post-production expertise.
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
