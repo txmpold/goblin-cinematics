121b9dacaed4424e9a1f28774b795b2abcaecf28
txmpold <love.tampold@hotmail.com>
2026-04-03 17:53:00 +0200

    add logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
+import goblin from "../assets/images/goblin.png";
+import "../pages/animation.css";
 import "./Footer.css";
 
 export default function Footer() {
   return (
-    <footer>
+    <footer className="slide-up">
+      <img src={goblin} alt="Goblin Cinematics Logo" />
       <p>© 2024 Goblin Cinematics. All rights reserved.</p>
     </footer>
   );
